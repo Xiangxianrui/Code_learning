6655b969bfd40fc9c1dc16d951ea9db019633261
Xiangxianrui <shengwan211@gmail.com>
2025-02-18 07:43:00 +0800

    增加了一个c语言目录，增加了一个c语言代码

---
+++ c/test1.c
@@ -0,0 +1,14 @@
+include <stdio.h>  // 正确写法，包含标准输入输出库
+
+// 语法错误：intmain() 应为 int main()，主函数需要空格
+int main() {
+    int a = 10;
+    short age = 20;
+    int high = 180;
+    float weight = 88.5;
+
+    // printf 格式错误：缺少 % 符号来打印变量 a 的值
+    printf("a=%d\n", a);  // 正确使用 %d 来打印整型变量 a
+
+    return 0;  // return 0 表示程序正常结束
+}
