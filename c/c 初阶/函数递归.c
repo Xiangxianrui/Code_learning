4a17600693b73a785cb296c562104142a4d7eb39
Xiangxianrui <shengwan211@gmail.com>
2025-03-10 18:52:51 +0800

    不知道改了啥

--- c/c 初阶/函数递归.c
+++ c/c 初阶/函数递归.c
@@ -16,8 +16,44 @@ int print_last(int num)
         print_last(num / 10);
     }
     printf("%d\n", num % 10);
+    return 0;
 }
+int jiechen(int n)
+{
+    if (n == 1)
+        return 1;
+    else
+        return n * jiechen(n - 1);
+}
+
+// 不使用递归计算斐波拉契数列的第 n 项
+
+int Feb2(int n)
+{
+
+    int a = 1;
+    int b = 1;
+    int c = 1;
+    while (n >= 3)
+    {
+        c = a + b;
+        a = b;
+        b = c;
+        c--;
+    }
+    return c;
+}
+
 #include <string.h>
+// 不使用临时变量，求解字符串的长度
+int My_strlen(char *str)
+{
+    if (*str != '\0')
+        return My_strlen(str + 1) + 1;
+    else
+        return 0;
+}
+
 // %d 打印有符号数   %u 打印无符号数
 int main()
 {
@@ -31,7 +67,9 @@ int main()
     int num = 0;
     printf("请输入一个数\n");
     scanf("%d", &num);
-    print_last(num);
+    // // print_last(num);
+    // MY_strlen();
+    printf("%d\n", jiechen(num));
 
     return 0;
 }
