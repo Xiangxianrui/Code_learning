61e56f64c694badd4b96ce6bc35d9d369b0aca51
Xiangxianrui <shengwan211@gmail.com>
2025-03-12 18:05:19 +0800

    数组的内容

---
+++ c/c 初阶/冒泡排序.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int arr[10] = {3, 5, 1, 7, 9, 8, 6, 4, 2, 0};
+    int sz = sizeof(arr) / sizeof([arr[0]]);
+}
+
+// void bubble_sort(int arr[],int size){
+//     int i = 0;
+//     for (i=0;i < size ;i++){
+//         if (arr[i] > arr[i+1]){
+//             int tmp = arr[i];
+
+//         }
+//     }
+// }
