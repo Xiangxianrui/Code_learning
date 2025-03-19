f15a0333457d7fc68dc547012c63010afbdc95fa
Xiangxianrui <shengwan211@gmail.com>
2025-03-19 19:26:13 +0800

    a

--- c/t/删除指定数字.c
+++ c/t/删除指定数字.c
@@ -1,1 +1,21 @@
 #include <stdio.h>
+void(char parr, int target, int sz)
+{
+    int slow = 0;
+    int fast = 0;
+    while (fast <)
+}
+
+int main()
+{
+    char arr[] = {0};
+    int target = 0;
+
+    printf("请输入一个整数字符串\n");
+    scanf("%s", arr);
+    printf("请输入要删除的数字(1-10):>");
+    int sz = strlen(arr);
+
+    remove(arr, target, sz);
+    printf("arr\n");
+}
