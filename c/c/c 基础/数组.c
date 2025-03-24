8071ffcbdec049f882a9b24735f975c252a17f39
Xiangxianrui <shengwan211@gmail.com>
2025-03-24 19:49:26 +0800

    不知道改了啥

--- c/c/c 基础/数组.c
+++ c/c/c 基础/数组.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-int main(){
-    int arr[10]={1,2,3,4,5,6,7,8,9,10};
+int main()
+{
+    int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
     int i = 0;
-    while (i<9){
-        printf("%d\t",arr[i]);
+    while (i < 9)
+    {
+        printf("%d\t", arr[i]);
         i++;
     }
-
-return 0;
+    return 0;
 }
