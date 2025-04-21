89484bd69043ce2d9ac13d70541b19847303d493
Xiangxianrui <shengwan211@gmail.com>
2025-04-21 15:30:28 +0800

    140，位断

--- c/进阶/字符串操作/自定义类型/结构体.c
+++ c/进阶/字符串操作/自定义类型/结构体.c
@@ -4,11 +4,24 @@ struct Stu
     char name[20];
     int age;
 
-}
+}; // 可以加上s1,s2
 
-int
-main()
+struct Node
 {
+    int data;
+    struct Node *next;
+};
 
+struct S1
+{
+    char c1;
+    int i;
+    char c2;
+};
+
+int main()
+{
+    // struct Stu s3;
+    printf("%d", sizeof(struct S1));
     return 0;
 }
---
+++ ttt/test.py
@@ -0,0 +1,2 @@
+for i in range(10,100,2):
+    print(3)
